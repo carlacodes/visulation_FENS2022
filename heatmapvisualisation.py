73973d2c477702798ddb3d9409361b40a38a7788
carlacodes <carla.griffiths@gmail.com>
2022-05-07 17:27:55 +0100

    removing non significant annotations thanks to stack overflow

--- heatmapvisualisation.py
+++ heatmapvisualisation.py
@@ -16,8 +16,20 @@ str = ['craft', 'in contrast to', 'when a', 'accurate', 'rev. target', 'of scien
        'instruments']
 channel_list=np.array([7,    8  ,   9  ,  10  ,  12   , 14    ,16   , 17  ,  26   , 27 ,   28  ,  30])
 for i in range(0, len(heatmap_bychan)):
-    selected_rect_data_raw=heatmap_bychan_raw[i]
+    selected_rect_data_raw=(heatmap_bychan_raw[i])
+
     selected_rect_data=heatmap_bychan[i]
+    index_x, index_y=np.where((selected_rect_data<=0))
+    show_annot_array = selected_rect_data >0
+    for k in range(0, len(index_x)):
+        index_x_selec=index_x[k]
+        index_y_s=index_y[k]
+        selected_rect_data_raw[index_x_selec][index_y_s]=np.empty(1)
+    #selected_rect_data_raw=np.array2string(selected_rect_data_raw, formatter={'float_kind': lambda x: "%.2f" % x})
+
     ax=sns.heatmap(selected_rect_data, xticklabels=str, yticklabels=str, cmap='Blues',annot=selected_rect_data_raw, vmin=0, vmax=0.2)
+    for text, show_annot in zip(ax.texts, (element for row in show_annot_array for element in row)):
+        text.set_visible(show_annot)
+
     plt.title('Site Number '+(np.array2string(channel_list[i])))
     plt.show()
