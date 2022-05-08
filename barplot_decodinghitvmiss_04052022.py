3303c9920128515cd813a8931ee1debc23c4db8f
carlacodes <carla.griffiths@gmail.com>
2022-05-08 12:44:16 +0100

    making raw boxplot with raw data

--- barplot_decodinghitvmiss_04052022.py
+++ barplot_decodinghitvmiss_04052022.py
@@ -39,46 +39,68 @@ matversion_difference=info_barplot['differencemat']
 # ax = sns.barplot([1,2,3], zola_y_l28,  color='green', label='F1702',alpha=.3)
 # ax = sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_eclair, axis=0), color='navy', alpha=0.3, label='F1902')
 # ax=sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_crumble, axis=0),  color='purple',  alpha=0.3, label='F1901')
-x_vec=np.empty((24,2))
-ax=sns.boxplot( y=matversion_difference[0])
-x_vecc1=np.transpose(np.array([0] * 12))
-x_vecc2=np.transpose(np.array([1] * 12))
-np.reshape(x_vecc1, (1,12), order='C')
-x_vec[0]=np.transpose(np.concatenate((x_vecc1, x_vecc2), axis=0))
-x_vec[1]=np.concatenate((matversion_difference[:,0], matversion_difference[:, 1]), axis=0)
-
-x_vec=x_vec.tolist()
-df = pd.DataFrame(matversion_difference, columns = ['Rove','Nonrove'])
+# x_vec=np.empty((24,2))
+# ax=sns.boxplot( y=matversion_difference[0])
+# x_vecc1=np.transpose(np.array([0] * 12))
+# x_vecc2=np.transpose(np.array([1] * 12))
+# np.reshape(x_vecc1, (1,12), order='C')
+# x_vec[0]=np.transpose(np.concatenate((x_vecc1, x_vecc2), axis=0))
+# x_vec[1]=np.concatenate((matversion_difference[:,0], matversion_difference[:, 1]), axis=0)
+#
+# x_vec=x_vec.tolist()
+df = pd.DataFrame(matversion_difference, columns = ['Rove','Natural'])
 
 ax=sns.boxplot(y=df["Rove"])
-plt.title('Change in decoding score between train and test dataset, inter-trial roving', fontsize=10)
+plt.title('Decoding score of behavioural response outcome, linear shift model', fontsize=10)
 str={'roved F0', 'control F0'}
 plt.xticks([0,1], labels=str, rotation=45)
-plt.xlabel('Distractor')
-plt.ylabel('Change in Mean Decoding Score for All Sites')
+plt.xlabel('F0 Type of Target Stimulus')
+plt.ylabel('Relative decoding score (score-chance)')
 
 plt.legend(fontsize=8)
-plt.ylim([-0.35, 0.35])
-plt.savefig(bin_folder + '\seabornbarplot_l28.png', bbox_inches='tight')
+plt.ylim([-0.35, 0.4])
+plt.savefig(bin_folder + '\seabornboxplotmissvhit_l27.png', bbox_inches='tight')
 ax = sns.boxplot(data=df, orient="y", palette="Set2")
+ax=sns.stripplot(data=df, jitter=0.1, palette='husl', alpha=1)
 plt.show()
 
 
-ax2 = plt.subplots()
-ax2=sns.stripplot(data=zola_y_l27_scatter, jitter=0.1, color='green', alpha=0.8)
-ax2=sns.stripplot(data=matversion_crumble_L27, jitter=0.1, color='purple', alpha=0.8)
-ax2=sns.stripplot(data=matversion_eclair_l27, jitter=0.1, color='navy', alpha=0.8)
-ax2 = sns.barplot([1,2,3,4,5,6,7], zola_y_l27_bar,  color='green', label='F1702',alpha=.3)
-ax2 = sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_eclair_l27, axis=0), color='navy', alpha=0.3, label='F1902')
-ax2=sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_crumble_L27, axis=0),  color='purple',  alpha=0.3, label='F1901')
-plt.xlabel('Distractor')
-str={'craft', "in contrast to", "when a", "accurate", "rev instruments", "of science", "pink noise instruments"}
-plt.xticks([0,1,2,3,4,5,6], labels=str, rotation=45)
-plt.ylabel('Change in Mean Decoding Score for All Sites')
+#barchartresults_orig
+fname='barchartresults_orig.mat'
+info_barplot = rd.loadmat(bin_folder + os.sep + fname)
+matversion_difference_orig=info_barplot['differencemat']
 
-plt.title('Change in decoding score between train and test dataset, intra-trial roving',fontsize=10)
+df_orig = pd.DataFrame(matversion_difference_orig, columns = ['Rove','Natural'])
+
+plt.title('Decoding score of behavioural response outcome, original data', fontsize=10)
+str={'roved F0', 'control F0'}
+plt.xticks([0,1], labels=str, rotation=45)
+plt.xlabel('F0 Type of Target Stimulus')
+plt.ylabel('Relative decoding score (score-chance)')
 
 plt.legend(fontsize=8)
-plt.ylim([-0.35, 0.35])
-plt.savefig(bin_folder + '\seabornbarplot_l27.png', dpi=500, bbox_inches='tight')
+plt.ylim([-0.35, 0.4])
+plt.savefig(bin_folder + '\seabornboxplothitvsmiss_origdata_l27.png', bbox_inches='tight')
+ax = sns.boxplot(data=df_orig, orient="y", palette="Set2")
+ax=sns.stripplot(data=df_orig, jitter=0.1, palette='husl', alpha=1)
 plt.show()
+
+
+#ax2 = plt.subplots()
+# ax2=sns.stripplot(data=zola_y_l27_scatter, jitter=0.1, color='green', alpha=0.8)
+# ax2=sns.stripplot(data=matversion_crumble_L27, jitter=0.1, color='purple', alpha=0.8)
+# ax2=sns.stripplot(data=matversion_eclair_l27, jitter=0.1, color='navy', alpha=0.8)
+# ax2 = sns.barplot([1,2,3,4,5,6,7], zola_y_l27_bar,  color='green', label='F1702',alpha=.3)
+# ax2 = sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_eclair_l27, axis=0), color='navy', alpha=0.3, label='F1902')
+# ax2=sns.barplot([1,2,3,4,5,6,7], np.mean(matversion_crumble_L27, axis=0),  color='purple',  alpha=0.3, label='F1901')
+# plt.xlabel('Distractor')
+# str={'craft', "in contrast to", "when a", "accurate", "rev instruments", "of science", "pink noise instruments"}
+# plt.xticks([0,1,2,3,4,5,6], labels=str, rotation=45)
+# plt.ylabel('Change in Mean Decoding Score for All Sites')
+#
+# plt.title('Change in decoding score between train and test dataset, intra-trial roving',fontsize=10)
+#
+# plt.legend(fontsize=8)
+# plt.ylim([-0.35, 0.35])
+# plt.savefig(bin_folder + '\seabornbarplot_l27.png', dpi=500, bbox_inches='tight')
+# plt.show()
