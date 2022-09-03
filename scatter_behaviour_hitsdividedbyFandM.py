5841749357d87d35ec0c1adefa26cfa627da1dbe
carlacodes <carla.griffiths@gmail.com>
2022-09-03 20:54:16 +0100

    giving up

--- scatter_behaviour_hitsdividedbyFandM.py
+++ scatter_behaviour_hitsdividedbyFandM.py
@@ -93,6 +93,12 @@ df_individual_zola = pd.DataFrame({'Control F0': zola_control_hit[0,:],
                     'Intra-trial Roved F0': zola_control_hit[1,:],'Inter-trial Roved F0': zola_control_hit[2,:], }, index=index)
 
 ax = df.plot.bar(rot=0)
+def jitter_dots(dots):
+    offsets = dots.get_offsets()
+    jittered_offsets = offsets
+    # only jitter in the x-direction
+    jittered_offsets[:, 0] += np.random.uniform(-0.3, 0.3, offsets.shape[0])
+    dots.set_offsets(jittered_offsets)
 #sns.pointplot(data=df_individual_macaroni,linestyles='', markers='o', ax=ax)
 markers_mac = {"Control F0": "X", "Intra-trial Roved F0": "X", 'Inter-trial Roved F0': 'X'}
 markers_tina = {"Control F0": "o", "Intra-trial Roved F0": "o", 'Inter-trial Roved F0': 'o'}
@@ -100,14 +106,54 @@ markers_cruella = {"Control F0": "^", "Intra-trial Roved F0": "^", 'Inter-trial 
 markers_zola = {"Control F0": "2", "Intra-trial Roved F0": "2", 'Inter-trial Roved F0': '2'}
 def jitter(values,j):
     return values + np.random.normal(j,0.1,values.shape)
-sns.scatterplot(data=df_individual_macaroni, legend=False, zorder=10, x_jitter=0.8,  markers=markers_mac, label='F2002')
-sns.scatterplot(data=df_individual_tina, legend=False, zorder=10, x_jitter=5,  markers=markers_tina, label='F1803')
-sns.scatterplot(data=df_individual_cruella, legend=False, zorder=10, x_jitter=5,  markers=markers_cruella, label='F1815')
-sns.scatterplot(data=df_individual_zola, legend=False, zorder=10, x_jitter=5,  markers=markers_zola, label='F1702')
+# tuples=('Control F0', 'Intra-trial Roved F0', 'Inter-trial Roved F0')
+# index2 = pd.MultiIndex.from_tuples(tuples, names=["first", "second"])
 
+#macplot=sns.stripplot(data=df_individual_macaroni.T,jitter=0.01, marker='X', label='F2002')
+# sns.scatterplot(data=df_individual_macaroni, legend=False, zorder=10, x_jitter=5,  markers=markers_mac, label='F2002')
+#
+# sns.scatterplot(data=df_individual_tina, legend=False, zorder=10, x_jitter=5,  markers=markers_tina, label='F1803')
+# sns.scatterplot(data=df_individual_cruella, legend=False, zorder=10, x_jitter=5,  markers=markers_cruella, label='F1815')
+# sns.scatterplot(data=df_individual_zola, legend=False, zorder=10, x_jitter=5,  markers=markers_zola, label='F1702')
+
+plt.scatter([-0.1],mac_control_hit[0,0],c='blue', label='F2002')
+plt.scatter([0.01],mac_control_hit[1,0], c='orange')
+plt.scatter([0.1],mac_control_hit[2,0], c='green')
+
+plt.scatter([1],mac_control_hit[0,1],c='blue')
+plt.scatter([1.1],mac_control_hit[1,1], c='orange')
+plt.scatter([1.2],mac_control_hit[2,1], c='green')
+
+plt.scatter([-0.15],tina_control_hit[0,0],c='blue', marker='X', label='F1803')
+plt.scatter([0.05],tina_control_hit[1,0], c='orange', marker='X')
+plt.scatter([0.15],tina_control_hit[2,0], c='green', marker='X')
+
+plt.scatter([0.8],tina_control_hit[0,1],c='blue', marker='X')
+plt.scatter([0.9],tina_control_hit[1,1], c='orange', marker='X')
+plt.scatter([1.1],tina_control_hit[2,1], c='green', marker='X')
+
+plt.scatter([-0.16],cruella_control_hit[0,0],c='blue', marker='^', label='F1815')
+plt.scatter([0.07],cruella_control_hit[1,0], c='orange',marker='^')
+plt.scatter([0.13],cruella_control_hit[2,0], c='green', marker='^')
+
+plt.scatter([0.85],cruella_control_hit[0,1],c='blue', marker='^')
+plt.scatter([0.95],cruella_control_hit[1,1], c='orange', marker='^')
+plt.scatter([1.15],cruella_control_hit[2,1], c='green', marker='^')
+
+plt.scatter([-0.14],zola_control_hit[0,0],c='blue', marker='s', label='F1702')
+plt.scatter([0.03],zola_control_hit[1,0], c='orange',marker='s',)
+plt.scatter([0.13],zola_control_hit[2,0], c='green', marker='s',)
+
+plt.scatter([0.85],zola_control_hit[0,1],c='blue', marker='s',)
+plt.scatter([0.95],zola_control_hit[1,1], c='orange', marker='s',)
+plt.scatter([1.15],zola_control_hit[2,1], c='green', marker='s',)
+
+
+tips = sns.load_dataset("tips")
 
 plt.ylabel('Proportion of Hits')
 plt.xlabel('Talker Sex')
 ax.set_ylim([0, 1])
-plt.legend()
+plt.legend(fontsize=8)
+plt.title('Proportion of Hits Over Talker Type')
 plt.show()
