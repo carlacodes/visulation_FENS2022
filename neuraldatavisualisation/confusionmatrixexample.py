824465b98c025d2a89b43d9400ff38c6b3eb77b9
carlacodes <carla.griffiths@gmail.com>
2022-09-05 12:31:16 +0100

    confusionmatrixexample.py added!!!

--- neuraldatavisualisation/confusionmatrixexample.py
+++ neuraldatavisualisation/confusionmatrixexample.py
@@ -15,6 +15,11 @@ import os
 import h5py
 import numpy as np
 
+from sklearn.datasets import make_classification
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
 
 list_of_distractors = [20, 57, 32, 56, 42, 2, 5]
 f={}
@@ -48,8 +53,8 @@ for i in list_of_distractors:
                 classifier_data[i] = arrays
                 count=count+1
 
-bigclassmat=np.array([])
-bigstimclasmat=np.array([])
+bigclassmat=np.zeros([1,12])
+bigstimclasmat=np.zeros([1,12])
 chansofinterest=[7,8,9, 10, 12, 14,16, 17, 26,  27, 28, 30]
 chansofinterest = [x - 1 for x in chansofinterest]
 for k in f:
@@ -58,3 +63,19 @@ for k in f:
     selectedclassmat_soundonset=selectedclassmat[:,chansofinterest]
     selectedstimmat=f[k]['stimclassmat']
     selectedstimclassmat_soundonset=selectedstimmat[:, chansofinterest]
+    bigclassmat=np.concatenate((bigclassmat, selectedclassmat_soundonset), axis=0)
+    bigstimclasmat=np.concatenate((bigstimclasmat, selectedstimclassmat_soundonset), axis=0)
+
+bigclassmat = np.delete(bigclassmat, 0, 0)
+bigstimclasmat= np.delete(bigstimclasmat, 0, 0)
+
+# meanbigclassmat=np.mean(bigclassmat, axis=1)
+# meanbigstimclassmat=np.mean(bigstimclasmat, axis=1)
+
+y_true=bigclassmat[:,0]
+y_true=bigclassmat.flatten()
+y_pred=bigstimclasmat[:,0]
+y_pred=bigstimclasmat.flatten()
+ax=ConfusionMatrixDisplay.from_predictions(y_true, y_pred, display_labels=['Distractor', 'Target'], cmap='Purples')
+plt.title('Confusion Matrix for Train on Control F0, Test on Roved F0 for\n all Channels with a Sound Onset Response')
+plt.show()
